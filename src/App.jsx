c92ca09530b23b05614d670c337a256d85d20a54
ahmukoma <ahmukoma@gmail.com>
2023-11-25 21:28:08 -0500

    skeleton, structure, form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import RoutesManager from './RoutesManager';
+import RoutesManager from './routes/RoutesManager';
 
 function App() {
   return (
--- src/routes/RoutesManager.jsx
+++ src/routes/RoutesManager.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./Header";
+import Header from "../Header";
 //import { Router, Routes, Route } from "routes";
 
 
